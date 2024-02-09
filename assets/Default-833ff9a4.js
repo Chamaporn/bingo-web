ab996ca5746ff0394167f8a958b8c8a3424323fa
benchamaporn <benchamaporn@NB00512>
2024-02-09 15:20:59 +0700

    add new voice file

---
+++ assets/Default-833ff9a4.js
@@ -0,0 +1,1 @@
+import{V as a,_ as t}from"./VApp-bf6e4cb5.js";import{o as e,c as o,w as r,a as s}from"./index-033204ae.js";import"./VMain-7ee3a39b.js";import"./tag-0a67549a.js";const l={__name:"Default",setup(_){return(c,p)=>(e(),o(a,null,{default:r(()=>[s(t)]),_:1}))}};export{l as default};
